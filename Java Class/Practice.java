82ca69c1a573edef7f22bec1b569581c9d64a5d2
Kolade Oluwadare <68949219+Accoladecreative@users.noreply.github.com>
2023-07-12 13:54:16 +0100

    Create Practice.java

---
+++ Java Class/Practice.java
@@ -0,0 +1,14 @@
+class Practice{
+  public static void main(String args[]){
+    System.out.println("Hello World");
+  
+Animal goat = new Animal(2, 4,"red","bingo");
+goat.setEyes(2);
+goat.getEyes();
+
+Animal lion = new Animal();
+Animal snake = new Animal();
+
+
+System.out.println(goat);
+}
